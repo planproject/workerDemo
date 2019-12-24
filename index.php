5f579aad5bc9439bc6a367c3049736dc52d99926
ferre <3178251769@qq.com>
2019-12-24 09:41:02 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2019/12/24
+ * Time: 9:33
+ */
+require_once __DIR__.'/vendor/autoload.php';
+use Workerman\Worker;
+// Create a Websocket server
+$ws_worker = new Worker("websocket://0.0.0.0:2346");
+
+// 4 processes
+$ws_worker->count = 4;
+
+// Emitted when new connection come
+$ws_worker->onConnect = function($connection)
+{
+    echo "New connection\n";
+};
+
+// Emitted when data received
+$ws_worker->onMessage = function($connection, $data)
+{
+    // Send hello $data
+    $connection->send('hello ' . $data);
+};
+
+// Emitted when connection closed
+$ws_worker->onClose = function($connection)
+{
+    echo "Connection closed\n";
+};
+
+// Run worker
+Worker::runAll();
